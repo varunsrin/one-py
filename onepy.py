7d2d0ce48c831d344477fd291255a05a19d950fb
varunsrin <varunsrin@gmail.com>
2011-06-06 22:40:50 -0700

    Switching to ElementTree from Minidom

--- onepy.py
+++ onepy.py
@@ -1,11 +1,13 @@
-import win32com.client
-from xml.dom import minidom
 import json
+import win32com.client
+import xml.etree.ElementTree
 
 onapp = win32com.client.gencache.EnsureDispatch('OneNote.Application')
 nb = onapp.GetHierarchy("",win32com.client.constants.hsPages)
-onetree = minidom.parseString(nb)
 
+
+NS = "{http://schemas.microsoft.com/office/onenote/2010/onenote}"
+oneTree = xml.etree.ElementTree.fromstring(nb)
 
 
 #Returns the Notebook Hierarchy as JSON
@@ -13,66 +15,72 @@ def getHierarchyJson():
     return(json.dumps(getHierarchy(), indent=4))
 
 
-#Returns the Notebook Hierarchy as a Dictionary
+# Returns the Notebook Hierarchy as a Dictionary Array  
 def getHierarchy():
     notebooks = []
-    for node in onetree.getElementsByTagName("one:Notebook"):
-        book = parseAttributes(node)
-        if node.hasChildNodes():
-            book['sections'], book['sectionGroups'] = parseChildren(node.childNodes)
-        notebooks.append(book)
+
+    for notebook in oneTree:
+        nbk = parseAttributes(notebook)
+        nbk['sections'], nbk['sectionGroups'] = getSections(notebook)
+        notebooks.append(nbk)
+
     return notebooks
 
 
 
+# Takes in a Notebook or SectionGroup  and returns a Dict Array of its Sections & Section Groups
 
-#Recursively returns Dictionary of Section Groups & Sections
-def parseChildren (nodelist):
-    sectionGroups = []
+def getSections(notebook):
     sections = []
-    for node in nodelist:
-        if (node.nodeName == 'one:SectionGroup'):
-            group = parseAttributes(node)
-            if node.hasChildNodes():
-                group['sections'],group['sectionGroups'] = parseChildren(node.childNodes)
-            sectionGroups.append(group)
-
-        if (node.nodeName == 'one:Section'):
-            sec = parseAttributes(node)
-            if node.hasChildNodes():
-                sec['pages'] = getPages(node.childNodes)
-            sections.append(sec)
-
+    sectionGroups = []
+    for section in notebook:
+        if (section.tag == NS + "SectionGroup"):
+            newSectionGroup = parseAttributes(section)
+            newSectionGroup['sections'], newSectionGroup['sectionGroups'] = getSections(section)
+            sectionGroups.append(newSectionGroup)   
+            
+        if (section.tag == NS + "Section"):
+            newSection = parseAttributes(section)
+            newSection['pages'] = getPages(section)
+            sections.append(newSection)
+            
     return sections, sectionGroups
 
-#Takes in a node and returns a dictionary of its attributes
-def parseAttributes (node):
+
+# Takes in a Section and returns a Dict Array of its Pages
+def getPages(section):
+     pages =[]
+     for page in section:
+         newPage = parseAttributes(page)
+         newPage['meta'] = getMeta(page)
+         pages.append(newPage)
+     return pages
+
+
+
+
+
+
+# Takes in a Page and returns a Dict Array of its Meta properties
+def getMeta (page):
+    metas = []
+    for meta in page:
+        metas.append(parseAttributes(meta))
+    return metas
+
+
+# Takes in an object and returns a dictionary of its values
+def parseAttributes(obj):
         tempDict = {}
-        for prop in node.attributes.values():
-            tempDict[prop.name] = prop.value
+        for key,value in obj.items():
+            tempDict[key] = value
         return tempDict
 
 
 
 
 
-#Returns Dictionary of Pages from a nodelist
-def getPages(nodelist):
-    pages = []
-    for node in nodelist:
-        page = parseAttributes(node)
-        if node.hasChildNodes():
-            page['meta'] = getMeta(node.childNodes)
-        pages.append(page)
-    return pages
 
-
-
-def getMeta (nodelist):
-    meta = []
-    for node in nodelist:
-        meta.append(parseAttributes(node))
-    return meta
 
 
 
